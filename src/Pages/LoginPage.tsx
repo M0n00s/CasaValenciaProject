a79e93c4dcf779e02959c50c05a7f9fec506dcee
M0n00s <programacion.buffalo@gmail.com>
2023-04-26 14:47:30 -0400

    done whit the login page a private routes

--- src/Pages/LoginPage.tsx
+++ src/Pages/LoginPage.tsx
@@ -1,7 +1,53 @@
-import React from 'react'
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { Toaster, toast } from "react-hot-toast";
 
-export const LoginPage = () => {
+import "./css/LoginPageCss.css";
+
+export const LoginPage = ({ setLogin }) => {
+  const navigate = useNavigate();
+  // input handler
+  const [code, setCode] = useState("");
+  const onInputChange = ({ target }) => {
+    setCode(target.value);
+  };
+  // login access
+  const onLogin = (e) => {
+    e.preventDefault();
+    if (code !== "2526") {
+      toast.error("wrong code, Please contact us");
+      setCode("");
+      return;
+    }
+    setLogin(true);
+    navigate("/");
+  };
   return (
-    <div>LoginPage</div>
-  )
-}
+    <div className="ContainerLogin">
+      <div>
+        <Toaster position="top-left" reverseOrder={false} />
+      </div>
+      <section>
+        <div>
+          <span>Casa Valencia Project</span>
+          <p className="titleLogin">Build beautiful products, faster.</p>
+        </div>
+
+        <div className="formLoginContainer">
+          <span>do you want to see the progress?</span>
+          <form onSubmit={onLogin}>
+            <input
+              type="text"
+              placeholder="ENTER YOUR CODE"
+              className="codeLogin"
+              name="code"
+              value={code}
+              onChange={onInputChange}
+              autoComplete="off"
+            />
+          </form>
+        </div>
+      </section>
+    </div>
+  );
+};
--- src/login/LoginRouter.tsx
+++ src/login/LoginRouter.tsx
@@ -1,24 +1,22 @@
 import { useState } from "react";
-import { Route, Routes, Navigate} from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import { AppPage } from "../Pages/AppPage";
 import { LoginPage } from "../Pages/LoginPage";
 
 export const LoginRouter = () => {
-  const [Login, setLogin] = useState(false)
+  const [Login, setLogin] = useState(false);
 
   return (
     <>
-    <Routes>
-      {
-        !Login ? 
-          <Route path="/login" element={<LoginPage />}/>
-        :
+      <Routes>
+        {!Login ? (
+          <Route path="/login" element={<LoginPage setLogin={setLogin} />} />
+        ) : (
           <Route path="/" element={<AppPage />} />
-      }
+        )}
 
-      <Route path="*" element={<Navigate to='login' />} />
-    </Routes>
-    
+        <Route path="*" element={<Navigate to="login" />} />
+      </Routes>
     </>
-  )
-}
+  );
+};
