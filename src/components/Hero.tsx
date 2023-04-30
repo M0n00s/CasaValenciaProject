236d63897f7f6404df88fd0792406d574b1a53cb
M0n00s <programacion.buffalo@gmail.com>
2023-04-30 17:35:32 -0400

    navbar responsive

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export const Hero = () => {
-  return <div className="h-[90vh] bg-red-300">Hero</div>;
+  return <div className="h-[90vh] ">Hero</div>;
 };
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,23 @@
-import React from "react";
 import { RiGovernmentLine } from "react-icons/ri";
+import { IoMdMenu, IoMdClose } from "react-icons/io";
 import { NavLink } from "react-router-dom";
+import { useState } from "react";
 
 export const Navbar = () => {
+  const [showMenu, setshowMenu] = useState(false);
   return (
-    <header className=" flex w-full items-center p-4 shadow-md h-[10vh]">
-      <div className=" w-1/6 flex items-center gap-2 text-primary">
+    <header className=" flex w-full items-center justify-between px-4 md:px-10 shadow-md h-[10vh]">
+      <div className="  flex items-center gap-2 text-primary">
         <span className="text-3xl">
           <RiGovernmentLine />
         </span>
-        <p className="font-bold whitespace-nowrap">Casa Valencia</p>
+        <p className="font-bold text-xl whitespace-nowrap">Casa Valencia</p>
       </div>
-      <nav className="flex gap-10 justify-center  flex-1">
+      <nav
+        className={` bg-white  pl-8 shadow-md flex flex-col gap-10 fixed w-[80%] h-full left-0 top-[11vh] transition-all duration-300  md:static md:items-center  md:flex-row  md:justify-center md:shadow-none md:pl-0 flex-1 ${
+          showMenu ? "left-0" : "-left-full"
+        }`}
+      >
         {["Home", "Habitaciones", "Contacto"].map((nav) => (
           <NavLink
             key={nav}
@@ -22,7 +28,15 @@ export const Navbar = () => {
           </NavLink>
         ))}
       </nav>
-      <div className="w-1/12 justify-center flex justify-end ">User</div>
+      <div className=" justify-end items-center  flex gap-2">
+        <div>User</div>
+        <button
+          onClick={() => setshowMenu(!showMenu)}
+          className=" text-3xl md:hidden"
+        >
+          {showMenu ? <IoMdClose /> : <IoMdMenu />}
+        </button>
+      </div>
     </header>
   );
 };
