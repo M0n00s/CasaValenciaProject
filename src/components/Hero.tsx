78481c5a9de5de900f33856d8c755e961a0e679e
M0n00s <programacion.buffalo@gmail.com>
2023-04-30 20:27:12 -0400

    remove svg waves

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,8 +3,8 @@ import { IoMdCall } from "react-icons/io";
 
 export const Hero = () => {
   return (
-    <div className=" hero  grid grid-cols-1 lg:grid-cols-8 overflow-x-hidden ">
-      <div className=" lg:col-span-5 flex flex-col  justify-center px-12 gap-8 py-8">
+    <div className=" hero  grid grid-cols-1 lg:grid-cols-8 overflow-x-hidden h-[90vh] ">
+      <div className=" lg:col-span-5 flex flex-col  justify-center px-12 gap-8 py-8 ">
         <h1 className=" text-5xl lg:text-7xl font-bold  ">
           Comodidad, atención y diversión al alcance de todos
         </h1>
@@ -28,15 +28,6 @@ export const Hero = () => {
           alt="img"
           className="w-[100%] h-[100%] object-contain "
         />
-      </div>
-      <div className="w-[100vw] mt-[-8rem]">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-          <path
-            fill="#ff6349"
-            fillOpacity="1"
-            d="M0,192L60,213.3C120,235,240,277,360,256C480,235,600,149,720,122.7C840,96,960,128,1080,128C1200,128,1320,96,1380,80L1440,64L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"
-          ></path>
-        </svg>
       </div>
     </div>
   );
