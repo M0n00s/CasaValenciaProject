6bb1d67a925311a2f615ac605ab62656bd37d4f8
M0n00s <programacion.buffalo@gmail.com>
2023-05-08 10:35:20 -0400

    change footer bg

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Footer = () => {
   return (
-    <div className="h-[30vh] bg-secondary flex items-center justify-center text-white font-bold text-xl">
+    <div className="h-[30vh] bg-primary flex items-center justify-center text-white font-bold text-xl">
       <p>Casa Valecia 2023 C.A.</p>
     </div>
   );
